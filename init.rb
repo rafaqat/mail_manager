87dc6c8b79d7cd966e34d03b6c4f12cb9a555dea
Christopher Louis Hauboldt <chris@hauboldt.us>
2013-10-30 08:19:58 -0500

    Conf fixes

--- init.rb
+++ init.rb
@@ -1,7 +1,4 @@
-unless defined?(MailManager::PLUGIN_ROOT)
-  # Include hook code here
-  require File.join(File.dirname(__FILE__), 'workers', 'mail_manager', 'bounce_job.rb')
-
+unless defined?(::MailManager::Conf)
   Rails.application.config.to_prepare do
     ApplicationController.helper(MailManager::SubscriptionsHelper)
     if defined?(::Conf)
--- lib/mail_manager.rb
+++ lib/mail_manager.rb
@@ -1,2 +1,5 @@
 require "mail_manager/version"
 require "mail_manager/engine"
+
+# Include hook code here
+require File.join(File.dirname(__FILE__), 'workers', 'mail_manager', 'bounce_job.rb')
