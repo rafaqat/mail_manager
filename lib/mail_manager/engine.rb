00ad39ef9bacb60e59859e73d5f34b56153a1305
Christopher Louis Hauboldt <chris@hauboldt.us>
2013-12-18 14:59:43 -0600

    more config settings

--- lib/mail_manager/engine.rb
+++ lib/mail_manager/engine.rb
@@ -4,26 +4,25 @@ module MailManager
     mattr_accessor :secret, :site_url, :dont_include_images_domains, :sleep_time_between_messages, :table_prefix,
       :default_from_email_address, :bounce, :unsubscribe_path, :site_path, :layout, :use_show_for_resources
     initializer "MailManager.config" do |app|
-      conf = if defined?(::Conf) && MailManager.respond_to?(:mail_manager)
-        ::Conf
-      else
+      if File.exist?('config/mail_manager.yml')
         require 'mail_manager/config'
-        MailManager::Config.initialize!
+        MailManager.initialize_with_config(MailManager::Config.initialize!)
       end
-      return unless conf.present?
-      MailManager.secret ||= conf.secret
+    end
+    def self.set_config(conf)
+      MailManager.secret ||= conf.secret rescue nil
       default_url_options = ActionController::Base.default_url_options
-      MailManager.site_url ||= conf.site_url || "#{default_url_options[:protocol]||'http'}://#{default_url_options[:domain]}"
-      MailManager.dont_include_images_domains ||= conf.dont_include_images_domains || []
-      MailManager.sleep_time_between_messages ||= conf.sleep_time_between_messages || 0.3
-      MailManager.table_prefix ||= conf.table_prefix || 'mail_manager_'
-      MailManager.default_from_email_address ||= conf.default_from_email_address
-      MailManager.bounce ||= conf.bounce || {}
-      MailManager.unsubscribe_path ||= conf.unsubscribe_path || "/listmgr"
-      MailManager.site_path ||= conf.site_path || "/"
-      MailMatager.layout ||= conf.layout || "application"
-      MailMatager.use_show_for_resources ||= conf.use_show_for_resources || false
-      end
+      default_site_url = "#{default_url_options[:protocol]||'http'}://#{default_url_options[:domain]}" 
+      MailManager.site_url ||= conf.site_url || default_site_url rescue default_site_url
+      MailManager.dont_include_images_domains ||= conf.dont_include_images_domains || [] rescue []
+      MailManager.sleep_time_between_messages ||= conf.sleep_time_between_messages || 0.3 rescue 0.3
+      MailManager.table_prefix ||= conf.table_prefix || 'mail_manager_' rescue 'mail_manager_'
+      MailManager.default_from_email_address ||= conf.default_from_email_address rescue nil
+      MailManager.bounce ||= conf.bounce || {} rescue {}
+      MailManager.unsubscribe_path ||= conf.unsubscribe_path || "/listmgr" rescue "/listmgr"
+      MailManager.site_path ||= conf.site_path || "/" rescue "/"
+      MailMatager.layout ||= conf.layout || "application" rescue "application"
+      MailMatager.use_show_for_resources ||= conf.use_show_for_resources || false rescue false
     end
   end
   PLUGIN_ROOT = File.expand_path(File.join(File.dirname(__FILE__),'..','..'))
