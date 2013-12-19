73b2ed87135aada2e2e80a67b1bc4c2720537dfd
Christopher Louis Hauboldt <chris@hauboldt.us>
2013-12-19 08:04:19 -0600

    Fixed contacts index... and subscription helper doesn't require default status

--- app/helpers/mail_manager/subscriptions_helper.rb
+++ app/helpers/mail_manager/subscriptions_helper.rb
@@ -1,6 +1,6 @@
 module MailManager
   module SubscriptionsHelper
-    def contactable_subscriptions_selector(contactable_form, default_unsubscribe_status)
+    def contactable_subscriptions_selector(contactable_form, default_unsubscribe_status="unsubscribed")
       render :partial => 'mail_mgr/subscriptions/subscriptions', :locals => {:contactable_form => contactable_form,
         :unsubscribed_status => default_unsubscribe_status}
     end
