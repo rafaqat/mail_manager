4d80d19070985fa78b9878c5d57af1e3ce09e886
Christopher Louis Hauboldt <chris@hauboldt.us>
2015-03-24 11:56:33 -0500

    user factory doesn't blow up when used in ireach(duplicate of mail_manager)

--- spec/test_app/spec/factories/users.rb
+++ spec/test_app/spec/factories/users.rb
@@ -1,10 +1,13 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :user do
-    first_name {Faker::Name.first_name}
-    last_name {Faker::Name.last_name}
-    email {Faker::Internet.email}
-    phone {Faker::PhoneNumber.phone_number}
+begin
+  FactoryGirl.define do
+    factory :user do
+      first_name {Faker::Name.first_name}
+      last_name {Faker::Name.last_name}
+      email {Faker::Internet.email}
+      phone {Faker::PhoneNumber.phone_number}
+    end
   end
+rescue FactoryGirl::DuplicateDefinitionError => e
+  # this is ok ... duplicates when iReach uses it
 end
