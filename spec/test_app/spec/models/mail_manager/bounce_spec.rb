af81c7611ac6d272b0a6ed3c35f43230e1f85254
Christopher Louis Hauboldt <chris@hauboldt.us>
2015-03-25 13:43:40 -0500

    fixed name of mailable spec; bounce job timing spec(also checks mail through test pop server)

--- spec/test_app/spec/models/mail_manager/bounce_spec.rb
+++ spec/test_app/spec/models/mail_manager/bounce_spec.rb
@@ -9,6 +9,24 @@ RSpec.describe MailManager::Bounce do
       MailManager::BounceJob.new.perform
       Delayed::Worker.delay_jobs = false
     end
+    it "should run every 10 minutes when there is mail on the current run" do
+      Delayed::Worker.delay_jobs = true
+      send_bounce('bad_utf8_chars.eml')
+      MailManager::BounceJob.new.perform
+      expect(Delayed::Job.count).to eq(1)
+      expect(Delayed::Job.first.run_at.utc.to_i).to be_within(5).of(
+        10.minutes.from_now.utc.to_i
+      )
+      Delayed::Worker.delay_jobs = false
+    end
+    it "should run every 120 minutes when there is no mail on the current check" do
+      Delayed::Worker.delay_jobs = true
+      MailManager::BounceJob.new.perform
+      expect(Delayed::Job.count).to eq(1)
+      expect(Delayed::Job.first.run_at.utc.to_i).to be_within(5).of(
+        120.minutes.from_now.utc.to_i
+      )
+    end
   end
   def send_bounce(filename)
     mail = Mail.new(File.read(File.join(Rails.root,'spec','support','files',filename)))
