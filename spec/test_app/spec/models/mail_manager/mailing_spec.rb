39240f69cc351f67b57a0ad6ed848ea60d40a839
Christopher Louis Hauboldt <chris@hauboldt.us>
2015-04-17 14:35:16 -0500

    Tested mailing include images logic (excluded email domains & the boolean on mailing)

--- spec/test_app/spec/models/mail_manager/mailing_spec.rb
+++ spec/test_app/spec/models/mail_manager/mailing_spec.rb
@@ -14,4 +14,34 @@ RSpec.describe MailManager::Mailing do
     expect(mailing.status.to_s).to eq('pending')
     expect(mailing.status_changed_at).not_to be nil
   end
+  it "doesn't include images when configured to not do so for a domain" do
+    # should be in config
+    image_url = "http://www.lone-star.net/graphics/lst_header_logo.png"
+    expect(MailManager.dont_include_images_domains).to include('yahoo.com')
+    mailing = FactoryGirl.create(:mailing, include_images: true)
+    mailing.mailable.update_attribute(:email_html, mailing.mailable.email_html.gsub(%r#file://.*/iReach_logo.gif#,image_url))
+    mailing.send_test_message('bobo@yahoo.com')
+    html_body = ActionMailer::Base.deliveries.last.parts.last.body
+    expect(html_body).to match /#{image_url}/
+    expect(html_body).not_to match %r#cid:#
+  end
+  it "doesn't include images when include_images is false" do
+    # should be in config
+    image_url = "http://www.lone-star.net/graphics/lst_header_logo.png"
+    expect(MailManager.dont_include_images_domains).to include('yahoo.com')
+    mailing = FactoryGirl.create(:mailing, include_images: false)
+    mailing.mailable.update_attribute(:email_html, mailing.mailable.email_html.gsub(%r#file://.*/iReach_logo.gif#,image_url))
+    mailing.send_test_message('bobo@example.com')
+    html_body = ActionMailer::Base.deliveries.last.parts.last.body
+    expect(html_body).to match /#{image_url}/
+    expect(html_body).not_to match %r#cid:#
+  end
+  it "does include images when its configuration doesn't exclude an email domain" do
+    image_url = "http://www.lone-star.net/graphics/lst_header_logo.png"
+    expect(MailManager.dont_include_images_domains).not_to include('example.com')
+    mailing = FactoryGirl.create(:mailing, include_images: true)
+    mailing.mailable.update_attribute(:email_html, mailing.mailable.email_html.gsub(%r#file://.*/iReach_logo.gif#,image_url))
+    mailing.send_test_message('bobo@example.com')
+    html_body = ActionMailer::Base.deliveries.last.to_s =~ /cid:=/
+  end
 end
